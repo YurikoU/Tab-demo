b817fcbfd72263664f7043937ff797e914aaf919
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-06-28 17:34:47 -0700

    Add files via upload

--- tab.js
+++ tab.js
@@ -1,44 +1,48 @@
-// IIFE, Immediately Invoked Function Expression, to prevent from polluting the global scope.
+// IIFE, Immediately Invoked Function Expression
 (() => {
+    //Declare the variables
     const $doc = document;
-    const $tab = $doc.getElementById('js-tab');
-    const $nav = $tab.querySelectorAll('[data-nav]');
-    const $content = $tab.querySelectorAll('[data-content]');
-    const ACTIVE_CLASS = 'is-active';
-    const navLen = $nav.length;
+    const $nav = $doc.querySelectorAll('[data-nav]');
+    const $content = $doc.querySelectorAll('[data-content]');
 
-    //初期化
+    const navLen = $nav.length;
+    const ACTIVE_CLASS = 'is-active';
+
+
+    //Initialize; display only the first content 
     const init = () => {
-        $content[0].style.display = 'block';//一つ目のコンテントを表示。
+        $content[0].style.display = 'block';
     };
     init();
 
 
-    //クリックしたら各コンテントを表示。
+    //Whole movement that behaves after clicking a tab
     const handleClick = (e) => {
-        e.preventDefault();//<a>のアンカーリンク要素を無効化。
-        
-        //クリックされたnavと、そのdata-属性を取得。
-        const $this = e.target;//クリックされた<a>を取得。
-        const targetVal = $this.dataset.nav;//<a>のdata-属性の値を取得。
-        
-        //対象外のnav、コンテンツをすべて一旦、リセット。
+        //Cancel the anchor link
+        e.preventDefault();
+
+        //Get the clicked HTML element and the value of the data- attribute
+        const $target = e.target;
+        const targetVal = $target.dataset.nav;
+
+        //Temporally remove the HTML class from all elements, and make all contents invisible 
         let index = 0;
         while (index < navLen) {
-            $content[index].style.display = 'none';
             $nav[index].classList.remove(ACTIVE_CLASS);
+            $content[index].style.display = 'none';
             index++;
         }
+          
+        //Make the HTML element visible that the value of the data-content attribute is matched with the data-nav
+        // $content[targetVal].style.display = 'block';
+        $doc.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';
 
-
-        //data-content属性が、定数targetValと一致するコンテンツが、表示させる。
-        $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';
-
-        //クリックされたタブに、HTMLクラス名を追加する。
+        //Add the HTML class only to the clicked element
         $nav[targetVal].classList.add(ACTIVE_CLASS);
     };
+    
 
-    //すべてのタブに、handleClick()を実装する。
+    //handleClick() works by clicking one of the tabs
     let index = 0;
     while (index < navLen) {
         $nav[index].addEventListener('click', (e) => handleClick(e));
@@ -46,7 +50,3 @@
     }
 
 })();
-
-
-
-
